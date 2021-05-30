8ef91053991a1f70502ec4b620cd4e57f39c470b
Gilles Gnacadja <gilles.gnacadja@gmail.com>
2021-05-29 22:17:51 -0700

    Test functionality now uses tables create from own JS library

--- predictive-vs-retrodictive.js
+++ predictive-vs-retrodictive.js
@@ -59,35 +59,69 @@ function pTPR_pFNR__to__rTPR_rFPR( odds_pred_neg_cl , pTPR , pFNR )
 }
 
 
-function test__rTPR_rFPR__to__pTPR_pFNR( n , rTPR , rFPR , id_display_area )
+function test__rTPR_rFPR__to__pTPR_pFNR( n , rTPR , rFPR , idArea1 , idArea2 )
 {
     const x = get_x(n) ;
     const prop_obsv_pos_cl = x.prop_pos_cl ;
     const odds_obsv_neg_cl = x.odds_neg_cl ;
     const [ pTPR , pFNR ] = rTPR_rFPR__to__pTPR_pFNR( odds_obsv_neg_cl , rTPR , rFPR ) ;
-    document.getElementById(id_display_area).innerHTML
-        = "n = " + n + "<br/><br/>" +
-          "prop_obsv_pos_cl = " + prop_obsv_pos_cl + "<br/>" +
-          "odds_obsv_neg_cl = " + odds_obsv_neg_cl + "<br/><br/>" +
-          "rTPR = " + rTPR + "<br/>" +
-          "rFPR = " + rFPR + "<br/><br/>" +
-          "pTPR = " + pTPR + "<br/>" +
-          "pFNR = " + pFNR ;
+    
+    document.getElementById(idArea1).innerHTML
+        = "n = " + n + "<br/>rTPR = " + rTPR + "<br/>rFPR = " + rFPR ;
+     
+    let col1_spec    = {} ;
+    col1_spec.header = "prop_obsv_pos_cl" ;
+    col1_spec.data   = prop_obsv_pos_cl ;
+    
+    let col2_spec    = {} ;
+    col2_spec.header = "odds_obsv_neg_cl" ;
+    col2_spec.data   = odds_obsv_neg_cl ;
+    
+    let col3_spec    = {} ;
+    col3_spec.header = "pTPR" ;
+    col3_spec.data   = pTPR ;
+    
+    let col4_spec    = {} ;
+    col4_spec.header = "pFNR" ;
+    col4_spec.data   = pFNR ;
+    
+    col_spec = [ col1_spec , col2_spec , col3_spec , col4_spec ] ;
+    
+    table = make_table_A(col_spec) ;
+    
+    document.getElementById(idArea2).appendChild(table) ;
 }
 
 
-function test__pTPR_pFNR__to__rTPR_rFPR( n , pTPR , pFNR , id_display_area )
+function test__pTPR_pFNR__to__rTPR_rFPR( n , pTPR , pFNR , idArea1 , idArea2 )
 {
     const x = get_x(n) ;
     const prop_pred_pos_cl = x.prop_pos_cl ;
     const odds_pred_neg_cl = x.odds_neg_cl ;
     const [ rTPR , rFPR ] = pTPR_pFNR__to__rTPR_rFPR( odds_pred_neg_cl , pTPR , pFNR ) ;
-    document.getElementById(id_display_area).innerHTML
-        = "n = " + n + "<br/><br/>" +
-          "prop_pred_pos_cl = " + prop_pred_pos_cl + "<br/>" +
-          "odds_pred_neg_cl = " + odds_pred_neg_cl + "<br/><br/>" +
-          "pTPR = " + pTPR + "<br/>" +
-          "pFNR = " + pFNR + "<br/><br/>" +
-          "rTPR = " + rTPR + "<br/>" +
-          "rFPR = " + rFPR ;
+    
+    document.getElementById(idArea1).innerHTML
+        = "n = " + n + "<br/>pTPR = " + pTPR + "<br/>pFNR = " + pFNR ;
+     
+    let col1_spec    = {} ;
+    col1_spec.header = "prop_pred_pos_cl" ;
+    col1_spec.data   = prop_pred_pos_cl ;
+    
+    let col2_spec    = {} ;
+    col2_spec.header = "odds_pred_neg_cl" ;
+    col2_spec.data   = odds_pred_neg_cl ;
+    
+    let col3_spec    = {} ;
+    col3_spec.header = "rTPR" ;
+    col3_spec.data   = rTPR ;
+    
+    let col4_spec    = {} ;
+    col4_spec.header = "rFPR" ;
+    col4_spec.data   = rFPR ;
+    
+    col_spec = [ col1_spec , col2_spec , col3_spec , col4_spec ] ;
+    
+    table = make_table_A(col_spec) ;
+    
+    document.getElementById(idArea2).appendChild(table) ;
 }
