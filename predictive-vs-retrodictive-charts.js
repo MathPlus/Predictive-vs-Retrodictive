0f4410d041bcd5e56d3de25210e9826fdec0147a
Gilles Gnacadja <gilles.gnacadja@gmail.com>
2021-05-31 23:12:42 -0700

    Still preparing for charts. Consolidated and simplified data generation code.

--- predictive-vs-retrodictive-charts.js
+++ predictive-vs-retrodictive-charts.js
@@ -7,57 +7,40 @@ function get_x(n)
     let x = {} ;
     x.prop_pos_cl = [] ;
     x.odds_neg_cl = [] ;
-    // x.odds_pos_cl = [] ;
     for ( let i = 0 ; i <= n ; i++ )
     {
         x.prop_pos_cl[i] = i / n ;
         x.odds_neg_cl[i] = ( n - i ) / i ;
-        // x.odds_pos_cl[i] = i / ( n - i ) ;
     }
     return x ;
 }
 
 
-function f( a , u )
+function f( x , r )
 {
-    let v = 1 / ( 1 + u * a ) ;
-    return v ;
+    let y = 1 / ( 1 + x * r ) ;
+    return y ;
 }
 
 
-function g( odds_neg_cl , rate1_in , rate2_in )
+function g( x , u , v )
 {
-    const rate_ratio = rate1_in / rate2_in ;
-    let rate_out = [] ;
-    for ( let i = 0 ; i < odds_neg_cl.length ; i++ )
+    const r = v / u ;
+    let y = [] ;
+    for ( let i = 0 ; i < x.length ; i++ )
     {
-        rate_out[i] = f( rate_ratio , odds_neg_cl[i] ) ;
+        y[i] = f( x[i] , r ) ;
     }
-    return rate_out ;
+    return y ;
 }
 
 
-function rTPR_rFPR__to__pTPR_pFNR( odds_obsv_neg_cl , rTPR , rFPR )
+function h( x , y1 , y2 )
 {
-    const rFNR = 1.0 - rTPR ;
-    const rTNR = 1.0 - rFPR ;
-    
-    const pTPR = g( odds_obsv_neg_cl , rFPR , rTPR ) ;
-    const pFNR = g( odds_obsv_neg_cl , rTNR , rFNR ) ;
-    
-    return [ pTPR , pFNR ] ;
-}
-
-
-function pTPR_pFNR__to__rTPR_rFPR( odds_pred_neg_cl , pTPR , pFNR )
-{
-    const pFPR = 1.0 - pTPR ;
-    const pTNR = 1.0 - pFNR ;
-    
-    const rTPR = g( odds_pred_neg_cl , pFNR , pTPR ) ;
-    const rFPR = g( odds_pred_neg_cl , pTNR , pFPR ) ;
-    
-    return [ rTPR , rFPR ] ;
+    let z = {} ;
+    z._1 = g( x , y1     , y2     ) ;
+    z._2 = g( x , 1 - y1 , 1 - y2 ) ;
+    return z ;
 }
 
 
@@ -66,7 +49,9 @@ function test__rTPR_rFPR__to__pTPR_pFNR( n , rTPR , rFPR , idArea1 , idArea2 )
     const x = get_x(n) ;
     const prop_obsv_pos_cl = x.prop_pos_cl ;
     const odds_obsv_neg_cl = x.odds_neg_cl ;
-    const [ pTPR , pFNR ] = rTPR_rFPR__to__pTPR_pFNR( odds_obsv_neg_cl , rTPR , rFPR ) ;
+    const pTPR_pFNR = h( odds_obsv_neg_cl , rTPR , rFPR ) ;
+    const pTPR = pTPR_pFNR._1 ;
+    const pFNR = pTPR_pFNR._2 ;
     
     document.getElementById(idArea1).innerHTML
         = "n = " + n + "<br/>rTPR = " + rTPR + "<br/>rFPR = " + rFPR ;
@@ -100,7 +85,9 @@ function test__pTPR_pFNR__to__rTPR_rFPR( n , pTPR , pFNR , idArea1 , idArea2 )
     const x = get_x(n) ;
     const prop_pred_pos_cl = x.prop_pos_cl ;
     const odds_pred_neg_cl = x.odds_neg_cl ;
-    const [ rTPR , rFPR ] = pTPR_pFNR__to__rTPR_rFPR( odds_pred_neg_cl , pTPR , pFNR ) ;
+    const rTPR_rFPR = h( odds_pred_neg_cl , pTPR , pFNR ) ;
+    const rTPR = rTPR_rFPR._1 ;
+    const rFPR = rTPR_rFPR._2 ;
     
     document.getElementById(idArea1).innerHTML
         = "n = " + n + "<br/>pTPR = " + pTPR + "<br/>pFNR = " + pFNR ;
