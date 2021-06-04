77f30d4868f2ba49f8246dec93a3e6052c2ce19b
Gilles Gnacadja <gilles.gnacadja@gmail.com>
2021-06-04 07:08:57 -0700

    Good progress in building chart

--- charting.js
+++ charting.js
@@ -9,6 +9,34 @@ function chartLoadCallback()
 }
 
 
+var chartOptions =
+{
+    'title'             : 'Loan Chart' ,
+    'curveType'         : 'function' ,
+    'hAxis'             : { 'title'          : 'x' ,
+                            'titleTextStyle' : { 'italic' : false ,
+                                                 'bold'   : true } ,
+                            'format'         : 'percent' ,
+                            'textStyle'      : { 'bold' : true } } ,
+    'vAxis'             : { 'format'    : 'percent' ,
+                            'textStyle' : { 'bold' : true } } ,
+    'legend'            : { 'position'  : 'top' ,
+                            'textStyle' : { 'bold' : true } } ,
+    'lineWidth'         : 3 ,
+    'colors'            : [ 'green' , 'red' ] ,
+    'chartArea'         : { 'width' : '80%' , 'height' : '80%' } ,
+    'selectionMode'     : 'multiple' ,
+    'tooltip'           : { 'trigger' : 'both' } ,
+    'aggregationTarget' : 'none' ,
+    'focusTarget'       : 'category' ,
+    'explorer'          : { 'axis'    : 'horizontal' ,
+                            'actions' : [ 'dragToZoom' , 'rightClickToReset' ] } ,
+    'crosshair'         : { 'trigger'     : 'both' ,
+                            'orientation' : 'vertical' ,
+                            'color'       : 'grey' }
+} ;
+
+
 function drawChart() 
 {
     var chartData = [ [ 'x' , 'z1' , 'z2' ] ] ;
@@ -22,25 +50,11 @@ function drawChart()
     }
 
     var dataTable = google.visualization.arrayToDataTable(chartData) ;
-
-    var chartOptions = {
-        'title'             : 'Loan Chart' ,
-        'curveType'         : 'function' ,
-        'legend'            : { 'position' : 'top' } ,
-        'chartArea'         : { 'width' : '80%' , 'height' : '80%' } ,
-        'selectionMode'     : 'multiple' ,
-        'tooltip'           : { 'trigger' : 'both'} ,
-        'aggregationTarget' : 'none' ,
-        'focusTarget'       : 'category' ,
-        'explorer'          : {
-                                  'axis'    : 'horizontal' ,
-                                  'actions' : ['dragToZoom', 'rightClickToReset']
-                              } ,
-        'crosshair'         : { 
-                                  'trigger'     : 'both' ,
-                                  'orientation' : 'vertical' 
-                              }
-    } ;
+    
+    var pct_formatter = new google.visualization.NumberFormat( { fractionDigits : 3 , pattern : '#.#%' } ) ;
+    pct_formatter.format( dataTable , 0 ) ;
+    pct_formatter.format( dataTable , 1 ) ;
+    pct_formatter.format( dataTable , 2 ) ;
 
     var dispArea = document.getElementById('retro-to-pred') ;
     var chart = new google.visualization.LineChart(dispArea) ;
